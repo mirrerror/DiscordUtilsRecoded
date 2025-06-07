0b923decc1d2d9dd8de32e276a81aa45daa8ce67
mirrerror <cayote20@mail.ru>
2025-06-08 01:56:56 +0300

    add mentions support to the chattodiscord system

--- src/main/java/md/mirrerror/discordutils/events/ChatToDiscordListener.java
+++ src/main/java/md/mirrerror/discordutils/events/ChatToDiscordListener.java
@@ -3,6 +3,11 @@ package md.mirrerror.discordutils.events;
 import club.minnced.discord.webhook.WebhookClient;
 import club.minnced.discord.webhook.WebhookClientBuilder;
 import club.minnced.discord.webhook.send.WebhookMessageBuilder;
+import md.mirrerror.discordutils.cache.DiscordUtilsUsersCacheManager;
+import md.mirrerror.discordutils.models.DiscordUtilsUser;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
@@ -25,10 +30,36 @@ public class ChatToDiscordListener implements Listener {
 
     @EventHandler
     public void onChat(AsyncPlayerChatEvent event) {
+        Player player = event.getPlayer();
+        DiscordUtilsUser discordUtilsUser = DiscordUtilsUsersCacheManager.getFromCacheByUuid(player.getUniqueId());
+        if (discordUtilsUser == null) return;
+
+        String message = event.getMessage();
+
+        if (!message.contains("@")) return;
+
+        StringBuilder messageBuilder = new StringBuilder();
+        String[] words = message.split(" ");
+        for (String word : words) {
+            if (word.startsWith("@")) {
+                String mentionedPlayerName = word.substring(1);
+                Player mentionedPlayer = Bukkit.getPlayer(mentionedPlayerName);
+                if (mentionedPlayer != null) return;
+                OfflinePlayer mentionedOfflinePlayer = Bukkit.getOfflinePlayer(mentionedPlayerName);
+                DiscordUtilsUser mentionedDiscordUtilsUser = DiscordUtilsUsersCacheManager.getFromCacheByUuid(mentionedOfflinePlayer.getUniqueId());
+                if (mentionedDiscordUtilsUser == null) return;
+                String mention = mentionedDiscordUtilsUser.getUser().getAsMention();
+                messageBuilder.append(mention);
+            } else {
+                messageBuilder.append(word);
+            }
+            messageBuilder.append(" ");
+        }
+
         WebhookMessageBuilder builder = new WebhookMessageBuilder();
         builder.setUsername(event.getPlayer().getName());
         builder.setAvatarUrl("https://mc-heads.net/avatar/" + event.getPlayer().getName());
-        builder.setContent(event.getMessage());
+        builder.setContent(messageBuilder.toString());
         client.send(builder.build());
     }
 
